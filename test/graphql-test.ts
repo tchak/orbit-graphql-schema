c347a1226f1b4a701dc0eefac486b3cab0c7fef7
Paul Chavard <git@paul.chavard.net>
2019-08-08 11:22:31 +0100

    Extract from orbit-server

---
+++ test/graphql-test.ts
@@ -0,0 +1,28 @@
+import { Schema } from '@orbit/data';
+import { GraphQLSchema } from 'graphql';
+
+import { makeExecutableSchema } from '../src';
+
+QUnit.module('makeExecutableSchema', function(hooks) {
+  let schema: Schema;
+  let graphqlSchema: GraphQLSchema;
+
+  hooks.beforeEach(async function() {
+    schema = new Schema({
+      models: {
+        user: {
+          attributes: {
+            name: { type: 'string' }
+          }
+        }
+      }
+    });
+
+    graphqlSchema = makeExecutableSchema(schema);
+  });
+
+  QUnit.test('it exists', function(assert) {
+    assert.ok(graphqlSchema);
+    assert.ok(graphqlSchema instanceof GraphQLSchema);
+  });
+});
